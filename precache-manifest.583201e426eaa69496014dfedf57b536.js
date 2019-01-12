b265afcc458e92bb3d612edcd83a4835fe86f8fa
seonghun <ao3oo3oa@gmail.com>
2019-01-12 15:22:03 +0900

    Updates

---
+++ precache-manifest.583201e426eaa69496014dfedf57b536.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c54ec0727b7919b84cd52a2efc47814",
+    "url": "/Choseonghun_Portfolio_1/static/media/github_icon.8c54ec07.png"
+  },
+  {
+    "revision": "c24830a0241204f9100406af3b1f24b4",
+    "url": "/Choseonghun_Portfolio_1/static/media/Home_Yakssok.c24830a0.png"
+  },
+  {
+    "revision": "4b98b8e112539f4c1e98c393723dde87",
+    "url": "/Choseonghun_Portfolio_1/static/media/Home_Syuya.4b98b8e1.png"
+  },
+  {
+    "revision": "87021259a63bb0d059c603e14ba79be1",
+    "url": "/Choseonghun_Portfolio_1/static/media/Home_Samgukji.87021259.png"
+  },
+  {
+    "revision": "0cc1740b27d790fd72c4e2d073ca906f",
+    "url": "/Choseonghun_Portfolio_1/static/media/Home_Library_web.0cc1740b.png"
+  },
+  {
+    "revision": "a6ac02128b2554190d0f",
+    "url": "/Choseonghun_Portfolio_1/static/js/runtime~main.a6ac0212.js"
+  },
+  {
+    "revision": "a1cd8a03b483d42efd22",
+    "url": "/Choseonghun_Portfolio_1/static/js/main.a1cd8a03.chunk.js"
+  },
+  {
+    "revision": "6ad0d733982fa6cf5d9c",
+    "url": "/Choseonghun_Portfolio_1/static/js/1.6ad0d733.chunk.js"
+  },
+  {
+    "revision": "a1cd8a03b483d42efd22",
+    "url": "/Choseonghun_Portfolio_1/static/css/main.4c9db74a.chunk.css"
+  },
+  {
+    "revision": "cb3d5f87634cea1dbdaaceea58bfba1b",
+    "url": "/Choseonghun_Portfolio_1/index.html"
+  }
+];
