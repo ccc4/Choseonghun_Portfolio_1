3e093524a3247d9ab65c65a7ceab361b840ba187
seonghun <ao3oo3oa@gmail.com>
2019-01-12 15:38:51 +0900

    update notice

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Projects from './Components/Projects';
 
 class App extends Component {
   render() {
+    alert("각 웹페이지는 데스크탑으로 볼 때 최적화되어 있습니다.\n모바일 최적화는 곧 적용될 예정입니다.");
     return (
       <div className="App">
         {/* <Header /> */}
